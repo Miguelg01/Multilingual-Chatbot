12d6350340267991d133efe1aef11a5041376f37
XIIFulminata <XIIFulminata@users.noreply.github.com>
2018-09-30 08:45:14 -0400

    Add files via upload

---
+++ twilioCredentials-redacted.py
@@ -0,0 +1,10 @@
+#fill in info, and leave in the same folder as run.py
+#while virtualenv and tunnel are up
+
+class Cred:
+    def __init__(self):
+        self.id='your_sid_here'
+        self.token='your_auth_token_here'
+        self.user_1_number='+10_digit_phone_number'
+        self.user_2_number='+10_digit_phone_number'
+        self.twilio_number='+10_digit_phone_number'
